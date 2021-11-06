f535a02d17a4fcbf06d90766c72f3611cd86ca96
Kevin Zhang <kevinleezhang@gmail.com>
2021-11-06 16:26:22 -0400

    Removed some unnecessary code.

--- scripts/point_cloud_to_voxel.py
+++ scripts/point_cloud_to_voxel.py
@@ -7,7 +7,7 @@ import numpy as np
 from sensor_msgs.msg import PointCloud2
 from utils import convertCloudFromRosToOpen3d
 from autolab_core import RigidTransform
-from voxel_msgs.msg import VoxelList,Voxel
+from voxel_msgs.msg import VoxelList
 
 def visualize(pcd_list):
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.6, origin=[0,0,0])
@@ -23,17 +23,7 @@ if __name__ == '__main__':
 
     rospy.init_node('voxel_publisher')
 
-    voxel_pub = rospy.Publisher('/voxels', VoxelList, queue_size=10)
-
-    def make_voxel(data):
-        v = Voxel()
-        v.x = data[0]
-        v.y = data[1]
-        v.z = data[2]
-        v.r = data[3]
-        v.g = data[4]
-        v.b = data[5]
-        return(v)
+    voxel_pub = rospy.Publisher('/voxels', VoxelList, queue_size=1)
 
     def publishVoxelList(pc2_msg):
         open3d_pc = convertCloudFromRosToOpen3d(pc2_msg)
@@ -48,6 +38,6 @@ if __name__ == '__main__':
         voxel_list_msg.data = voxel_list.flatten()
         voxel_pub.publish(voxel_list_msg)
 
-    rospy.Subscriber("/points2", PointCloud2, publishVoxelList)
+    rospy.Subscriber("/points2", PointCloud2, publishVoxelList, queue_size=1)
 
     rospy.spin()  
